bfef42983249bf1a03529979c8069975ebf740c9
AlanCobenaN <alanjoel311@gmail.com>
2026-06-18 03:21:09 -0500

    fix: agrega migracion faltante para valores del enum NotificationType (amistad_enviada, respuesta_post, respuesta_comment)

---
+++ kuroshi_app/backend/prisma/migrations/20260618083000_add_notification_types/migration.sql
@@ -0,0 +1,6 @@
+-- AlterEnum
+ALTER TYPE "NotificationType" ADD VALUE 'amistad_enviada';
+-- AlterEnum
+ALTER TYPE "NotificationType" ADD VALUE 'respuesta_post';
+-- AlterEnum
+ALTER TYPE "NotificationType" ADD VALUE 'respuesta_comment';
