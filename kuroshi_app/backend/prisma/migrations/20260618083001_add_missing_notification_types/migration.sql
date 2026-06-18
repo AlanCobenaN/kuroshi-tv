78fb630e6442e71f32505a3ec23be2e36b7e6d12
AlanCobenaN <alanjoel311@gmail.com>
2026-06-18 03:28:09 -0500

    fix: migracion solo con valores faltantes (respuesta_post, respuesta_comment)

--- kuroshi_app/backend/prisma/migrations/20260618083001_add_missing_notification_types/migration.sql
+++ kuroshi_app/backend/prisma/migrations/20260618083001_add_missing_notification_types/migration.sql
@@ -1,6 +1,4 @@
 -- AlterEnum
-ALTER TYPE "NotificationType" ADD VALUE 'amistad_enviada';
--- AlterEnum
 ALTER TYPE "NotificationType" ADD VALUE 'respuesta_post';
 -- AlterEnum
 ALTER TYPE "NotificationType" ADD VALUE 'respuesta_comment';
